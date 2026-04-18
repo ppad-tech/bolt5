8f31b418d0ca16e20b59bb393c700bf309453ea0
Jared Tobin <jared@jtobin.io>
2026-04-18 21:14:35 +0800

    Add bolt3, tx, and transitive deps to flake and cabal
    Add ppad-bolt3, ppad-tx, ppad-sha256, ppad-secp256k1, and
    ppad-ripemd160 as flake inputs with proper follows references.
    Update cabal build-depends for the library (ppad-bolt3,
    ppad-sha256, ppad-tx), test suite (ppad-bolt3, ppad-tx,
    tasty-quickcheck, QuickCheck), and benchmarks (ppad-bolt3,
    ppad-tx).
    
    Add BOLT5.Detect, BOLT5.Spend, BOLT5.Types to exposed-modules.

--- flake.nix
+++ flake.nix
@@ -7,11 +7,49 @@
       url  = "git://git.ppad.tech/nixpkgs.git";
       ref  = "master";
     };
+    ppad-sha256 = {
+      type = "git";
+      url  = "git://git.ppad.tech/sha256.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+    };
+    ppad-secp256k1 = {
+      type = "git";
+      url  = "git://git.ppad.tech/secp256k1.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+      inputs.ppad-sha256.follows = "ppad-sha256";
+    };
+    ppad-ripemd160 = {
+      type = "git";
+      url  = "git://git.ppad.tech/ripemd160.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+    };
+    ppad-tx = {
+      type = "git";
+      url  = "git://git.ppad.tech/tx.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+      inputs.ppad-sha256.follows = "ppad-sha256";
+    };
+    ppad-bolt3 = {
+      type = "git";
+      url  = "git://git.ppad.tech/bolt3.git";
+      ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+      inputs.ppad-sha256.follows = "ppad-sha256";
+      inputs.ppad-secp256k1.follows = "ppad-secp256k1";
+      inputs.ppad-ripemd160.follows = "ppad-ripemd160";
+      inputs.ppad-tx.follows = "ppad-tx";
+    };
     flake-utils.follows = "ppad-nixpkgs/flake-utils";
     nixpkgs.follows = "ppad-nixpkgs/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ppad-nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, ppad-nixpkgs
+            , ppad-sha256, ppad-secp256k1, ppad-ripemd160
+            , ppad-tx, ppad-bolt3 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         lib = "ppad-bolt5";
@@ -21,7 +59,34 @@
         llvm  = pkgs.llvmPackages_19.llvm;
         clang = pkgs.llvmPackages_19.clang;
 
+        sha256 = ppad-sha256.packages.${system}.default;
+        sha256-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag sha256 "llvm")
+            [ llvm clang ];
+
+        secp256k1 = ppad-secp256k1.packages.${system}.default;
+        secp256k1-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag secp256k1 "llvm")
+            [ llvm clang ];
+
+        ripemd160 = ppad-ripemd160.packages.${system}.default;
+        ripemd160-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag ripemd160 "llvm")
+            [ llvm clang ];
+
+        tx = ppad-tx.packages.${system}.default;
+
+        bolt3 = ppad-bolt3.packages.${system}.default;
+
         hpkgs = pkgs.haskell.packages.ghc910.extend (new: old: {
+          ppad-sha256 = sha256-llvm;
+          ppad-secp256k1 = secp256k1-llvm;
+          ppad-ripemd160 = ripemd160-llvm;
+          ppad-tx = tx;
+          ppad-bolt3 = bolt3;
           ${lib} = new.callCabal2nix lib ./. { };
         });
 
